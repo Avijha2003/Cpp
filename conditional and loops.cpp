a08b90d7484bad52478f556f38eccd739c3bff8f
Avijha2003 <jhaabhishekkumar49@gmail.com>
2024-09-19 20:39:34 +0530

    message

---
+++ conditional and loops.cpp
@@ -0,0 +1,80 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+/*
+ int a;
+   cin>>a;
+
+   //cout<< "value of n is:" << n <<endl;
+
+   //If a is positive
+   if(a>0) {
+    cout<<"A is positive"<<endl;
+   }
+   
+     else{
+        cout<< "A is negative"<<endl;
+     }
+
+
+    int a,b;
+  //  cin>> a >>b;
+
+   a = cin.get();
+
+   // cout<<" value of a and b is"<<a <<" "<<b <<endl;
+
+   cout<< "value of a is:"<<a <<endl;
+
+   int a,b;
+
+   cout<<"enter the value of a"<<endl;
+   cin>>a;
+   cout<<"enter the value of b"<<endl;
+   cin>>b;
+
+     if(a>b) {
+        cout<<"A is greater" <<endl;
+     }
+   if (b>a) {
+      cout<<"b is greater"<<endl;
+   }
+
+
+  
+   int a ;
+   cout<<"enter the value of a" <<endl;
+   cin>>a;
+    
+    if(a>0) {
+      cout<<"A is positive";
+    }
+    else{
+      if (a<0) {
+         cout<<"A is negative" ;
+      }
+         else{
+            cout<<"A is 0";
+     }
+}   
+
+  
+    int a ;
+   cout<<"enter the value of a" <<endl;
+   cin>>a;
+
+   if(a>0){
+      cout<<"A is positive"<<endl;
+   }
+     else if(a<0){
+      cout<<"A is negative"<<endl;
+     }
+    else{
+      cout<<"A id 0"<<endl;
+    }
+    
+*/
+    
+}
---
+++ first.cpp
@@ -0,0 +1,166 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+/* 
+
+    cout<<"Namestey dunia"<<endl;
+
+ 
+    int a=123;
+    cout<< a <<endl; 
+
+    char b = 'v';
+        
+    cout<< b <<endl; 
+
+    bool bl = true;
+    cout<< bl <<endl;
+
+    float f =1.2;
+    cout<< f <<endl;
+
+    double d = 1.234;
+    cout<<d<<endl;
+
+    int size = sizeof (a);
+    cout<<"sizw of a is :" <<size<<endl;
+ 
+   
+   int a = 'a';
+   cout<< a <<endl; 
+
+   char ch = 98;
+    cout<< ch <<endl;
+
+    char ch1 = 123456;
+    cout<< ch1 <<endl;
+
+
+    unsigned int a=112;
+
+    cout<< a <<endl;
+
+    int a = 2.0/5;
+    cout << a <<endl;
+
+    cout<< 2.0/5 <<endl;
+
+    float a = 2.0/5;
+
+    cout<< a <<endl;
+
+    cout<< 2.0/5<<endl;
+ 
+    double a= 2.0/5;
+
+    cout << a <<endl;
+
+    cout<< 2.0/5 <<endl;
+
+    int a = 2;
+    int b = 3;
+
+    bool first = (a==b);
+    cout<< first<<endl;
+
+     bool second = (a>b);
+    cout<< second<<endl;
+
+    bool third = (a<b);
+    cout<< third<<endl;
+
+
+   bool fourth = (a<=b);
+    cout<< fourth<<endl;
+
+
+    bool fifth = (a>=b);
+    cout<< fifth<<endl;
+
+
+    bool sixth = (a!=b);
+    cout<<sixth<<endl;
+
+    
+   int a = 23;
+   cout<< !a<<endl;
+
+   int a = 0;
+   cout<< !a <<endl;
+
+
+   int a;
+   cin>>a;
+
+   //cout<< "value of n is:" << n <<endl;
+
+   //If a is positive
+   if(a>0) {
+    cout<<"A is positive"<<endl;
+   }
+   
+     else{
+        cout<< "A is negative"<<endl;
+     }
+
+
+    int a,b;
+  //  cin>> a >>b;
+
+   a = cin.get();
+
+   // cout<<" value of a and b is"<<a <<" "<<b <<endl;
+
+   cout<< "value of a is:"<<a <<endl;
+
+   int a,b;
+
+   cout<<"enter the value of a"<<endl;
+   cin>>a;
+   cout<<"enter the value of b"<<endl;
+   cin>>b;
+
+     if(a>b) {
+        cout<<"A is greater" <<endl;
+     }
+   if (b>a) {
+      cout<<"b is greater"<<endl;
+   }
+
+
+  
+   int a ;
+   cout<<"enter the value of a" <<endl;
+   cin>>a;
+    
+    if(a>0) {
+      cout<<"A is positive";
+    }
+    else{
+      if (a<0) {
+         cout<<"A is negative" ;
+      }
+         else{
+            cout<<"A is 0";
+     }
+}   
+
+  
+    int a ;
+   cout<<"enter the value of a" <<endl;
+   cin>>a;
+
+   if(a>0){
+      cout<<"A is positive"<<endl;
+   }
+     else if(a<0){
+      cout<<"A is negative"<<endl;
+     }
+    else{
+      cout<<"A id 0"<<endl;
+    }
+
+    */
+}
